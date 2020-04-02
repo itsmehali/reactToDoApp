760123505e3b6ef8218fdbe8c8b10233740caf44
itsmehali <halaszbalazs9825@gmail.com>
2020-04-02 22:16:39 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import TodoItem from "./components/TodoItem";
+import todosData from "./todosData";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
+    // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos
+      };
+    });
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return (
+      <div>
+        <div className="todo-list">{todoItems}</div>;
+      </div>
+    );
+  }
+}
+
+export default App;
