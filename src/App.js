4c0cc8dc2458376699c1d4df9f51b7742ea86e6b
itsmehali <halaszbalazs9825@gmail.com>
2020-04-06 18:12:56 +0200

    styling for the completed tasks

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      todos: todosData
+      todos: todosData,
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -15,24 +15,24 @@ class App extends Component {
   handleChange(id) {
     // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
     // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
-    this.setState(prevState => {
-      const updatedTodos = prevState.todos.map(todo => {
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map((todo) => {
         if (todo.id === id) {
           return {
             ...todo,
-            completed: !todo.completed
+            completed: !todo.completed,
           };
         }
         return todo;
       });
       return {
-        todos: updatedTodos
+        todos: updatedTodos,
       };
     });
   }
 
   render() {
-    const todoItems = this.state.todos.map(item => (
+    const todoItems = this.state.todos.map((item) => (
       <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
     ));
 
